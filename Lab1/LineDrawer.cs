3ce09a1222f6b0ad2bb7eb87d27c639470fef84a
Konstantin <f1nzer@outlook.com>
2013-07-15 01:39:14 +0800

    added brezenham & wu algs

---
+++ Lab1/LineDrawer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Lab1
+{
+    public class LineDrawer
+    {
+        public int PixelSize { get; set; }
+
+        private readonly WriteableBitmap _bmp;
+        public LineDrawer(WriteableBitmap bmp)
+        {
+            _bmp = bmp;
+        }
+
+        private void DrawPixel(int x, int y)
+        {
+            _bmp.FillRectangle(x, y, x + PixelSize, y + PixelSize, Colors.Black);
+        }
+
+        private void DrawPixel(bool steep, int x, int y, float intense)
+        {
+            if (steep) Swap(ref x, ref y);
+
+            var colorIntense = Convert.ToByte(255*intense);
+            var color = Color.FromArgb(colorIntense, 0, 0, 0);
+            _bmp.SetPixel(x, y, color);
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            var z = a;
+            a = b;
+            b = z;
+        }
+
+        public void BresenhamLine(int x1, int y1, int x2, int y2)
+        {
+            var steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1); // Проверяем рост отрезка по оси x и по оси y
+            // Отражаем линию по диагонали, если угол наклона слишком большой
+            if (steep)
+            {
+                Swap(ref x1, ref y1);
+                Swap(ref x2, ref y2);
+            }
+            // Если линия растёт не слева направо, то меняем начало и конец отрезка местами
+            if (x1 > x2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+            int dx = x2 - x1;
+            int dy = Math.Abs(y2 - y1);
+            int error = dx / 2; // Оптимизация с умножением на dx, чтобы избавиться от лишних дробей
+            int ystep = (y1 < y2) ? 1 : -1; // Выбираем направление роста координаты y
+            int y = y1;
+            for (int x = x1; x <= x2; x++)
+            {
+                DrawPixel(steep ? y : x, steep ? x : y); // Возвращаем координаты на место
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+        }
+
+        public void WuLine(int x1, int y1, int x2, int y2)
+        {
+            var steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+            if (steep)
+            {
+                Swap(ref x1, ref y1);
+                Swap(ref x2, ref y2);
+            }
+            if (x1 > x2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+
+            DrawPixel(steep, x1, y1, 1);
+            DrawPixel(steep, x2, y2, 1);
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+            float gradient = dy / dx;
+            float y = y1 + gradient;
+            for (var x = x1 + 1; x <= x2 - 1; x++)
+            {
+                DrawPixel(steep, x, (int)y, 1 - (y - (int)y));
+                DrawPixel(steep, x, (int)y + 1, y - (int)y);
+                y += gradient;
+            }
+        }
+    }
+}
---
+++ Lab1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly WriteableBitmap _bmp;
+        private Point _fromPoint;
+        private readonly LineDrawer _lineDrawer;
+        private bool _isLeftBtnPressed = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _bmp = BitmapFactory.New(640, 480);
+            _bmp.Clear();
+            image.Source = _bmp;
+            _lineDrawer = new LineDrawer(_bmp);
+
+            image.MouseLeftButtonDown += ImageOnMouseLeftButtonDown;
+            image.MouseLeftButtonUp += ImageOnMouseLeftButtonUp;
+            MouseMove += OnMouseMove;
+        }
+
+        private void ImageOnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _isLeftBtnPressed = false;
+        }
+
+        private void ImageOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _isLeftBtnPressed = true;
+            _fromPoint = mouseButtonEventArgs.GetPosition(image);
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (_isLeftBtnPressed == false) return;
+            var point = mouseEventArgs.GetPosition(image);
+            if (point.X >= image.ActualWidth || point.X <= 0 || point.Y >= image.ActualHeight || point.Y <= 0) return;
+            _bmp.Clear();
+            int x1 = Convert.ToInt16(_fromPoint.X), y1 = Convert.ToInt16(_fromPoint.Y);
+            int x2 = Convert.ToInt16(point.X), y2 = Convert.ToInt16(point.Y);
+
+            if (cbAlgType.SelectedIndex == 0)
+            {
+                _lineDrawer.BresenhamLine(x1, y1, x2, y2);
+            }
+            else _lineDrawer.WuLine(x1, y1, x2, y2);
+        }
+    }
+}
