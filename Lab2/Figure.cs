144552ae22a5835aa38ca26790c4b623a82278cc
Konstantin <f1nzer@outlook.com>
2013-07-15 17:12:06 +0800

    skeleton for lab2

---
+++ Lab2/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Lab2
+{
+    public class Figure : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
